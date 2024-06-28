723268c1c28a4579c9b1c55c067ed9d222a229ed
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-06-28 14:34:45 -0400

    Update backend.tf

--- 08 - Terraform backend/Remote state Storage/backend-for-ec2/backend.tf
+++ 08 - Terraform backend/Remote state Storage/backend-for-ec2/backend.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
 
 
   backend "s3" {
-    bucket = "my-terraformstate-landmark-buc"
-    key    = "terraform/terraform.tfstate"
-    #dynamodb_table = "terraform-lock"
+    bucket = "bootcamp32-79-ken"    #this is d bucket we created locally
+    key    = "dev/terraform.tfstate"  #dev is like d directory where we want to store our statefile - terraform.tfstate
+    #dynamodb_table = "terraform-lock"   #it is good to pass this key (dev) for d dir, since i want to store multiple statefile
     region = "us-west-2"
 
   }
@@ -21,3 +21,21 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+to check what is in d statefile run: terraform state list
+
+monka@DESKTOP-0PEDM4P MINGW64 ~/terra today/Project-03/backend (master)
+$ terraform state list
+aws_dynamodb_table.tf_lock   # d name of d locke was terraform-lock
+aws_kms_key.mykey
+aws_s3_bucket.backend[0]
+aws_s3_bucket_server_side_encryption_configuration.backend   
+aws_s3_bucket_versioning.versioning_example
+random_integer.s3
+
+
+
+
+
+
+
