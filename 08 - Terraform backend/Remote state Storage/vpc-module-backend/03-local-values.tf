cab7f66b8580bc5e0746b1d89cc2b09d171ce978
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-06-28 16:12:50 -0400

    Update 03-local-values.tf

--- 08 - Terraform backend/Remote state Storage/vpc-module-backend/03-local-values.tf
+++ 08 - Terraform backend/Remote state Storage/vpc-module-backend/03-local-values.tf
@@ -1,8 +1,10 @@
 # Define Local Values in Terraform
+next goto 04
+
 locals {
   owners      = var.business_division
   environment = var.environment
-  name        = "${var.business_division}-${var.environment}"
+  name        = "${var.business_division}-${var.environment}"  #d name is a concatination of this business_division and d enviroment
   #name = "${local.owners}-${local.environment}"
   common_tags = {
     owners      = local.owners
