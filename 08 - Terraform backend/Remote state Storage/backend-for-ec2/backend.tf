ce429c058f8dcb9faffd6195e4412587781e9947
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-06-28 14:56:10 -0400

    Update backend.tf

--- 08 - Terraform backend/Remote state Storage/backend-for-ec2/backend.tf
+++ 08 - Terraform backend/Remote state Storage/backend-for-ec2/backend.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket = "bootcamp32-79-ken"    #this is d bucket we created locally
     key    = "dev/terraform.tfstate"  #dev is like d directory where we want to store our statefile - terraform.tfstate
-    #dynamodb_table = "terraform-lock"   #it is good to pass this key (dev) for d dir, since i want to store multiple statefile
+    dynamodb_table = "terraform-lock"   #it is good to pass this key (dev) for d dir, since i want to store multiple statefile
     region = "us-west-2"
 
   }
@@ -32,6 +32,27 @@ aws_s3_bucket.backend[0]
 aws_s3_bucket_server_side_encryption_configuration.backend   
 aws_s3_bucket_versioning.versioning_example
 random_integer.s3
+
+terraform init
+Initializing backend
+Successfully configured d be "s3"!  terra will use this be unless d be configuration changes.
+so this back end has been created.
+
+when u run d terraform apply it creates d ec2 instance desired
+when it finishes, it releases d state log. when it was provisioning d log was on.
+goto inside d bucket- u see d dev dir createde.
+if u click on it u have d terraform.tfstate file.
+click on it and open, it shows d statefile (w is inside d dir dev). this statefile is 4 this ec2 instance i just created.
+this bucket has been used to store my statefile.
+
+
+
+
+
+
+
+
+
 
 
 
